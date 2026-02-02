4207956f7f21344e222b58222141e61e5c1036cb
Thong Huynh <137142404+Thuynh808@users.noreply.github.com>
2026-02-02 00:35:14 -0600

    Add files via upload

---
+++ Windows-11-STIG-V2R5/__remediation_template(STIG-ID-WN11-AU-000054).ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Enables Auditing Account Lockout - Failures
+
+.NOTES
+    Author          : Thong Huynh
+    LinkedIn        : linkedin.com/in/thuynh808/
+    GitHub          : github.com/thuynh808
+    Date Created    : 2026-02-01
+    Last Modified   : 2026-02-01
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000054
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000054).ps1 
+#>
+
+auditpol /set /subcategory:"Account Lockout" /failure:enable
+
+# Quick verify
+auditpol /get /subcategory:"Account Lockout"
